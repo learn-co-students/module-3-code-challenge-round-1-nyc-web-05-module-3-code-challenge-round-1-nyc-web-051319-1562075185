0633ea43e52f7eb2801deac62abf4aeeea795c65
Amy Cen <cenamy@gmail.com>
2019-07-02 10:29:19 -0400

    done

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const BEERS_URL = "http://localhost:3000/beers"
+const beerList = document.querySelector("#list-group")
+
+function getBeers() {
+    fetch(BEERS_URL)
+    .then(resp => resp.json())
+    .then(beers => {
+        const beerList = document.querySelector("#list-group")
+        beers.forEach(beer => {
+            beerList.innerHTML += `<li class="list-group-item" data-id="${beer.id}">${beer.name}</li>`
+        })
+    })
+}
+
+document.addEventListener("DOMContentLoaded", getBeers)
+
+beerList.addEventListener("click", showBeer)
+
+function showBeer(e) {
+    const beerId = e.target.dataset.id
+    fetch(`${BEERS_URL}/${beerId}`)
+    .then(resp => resp.json())
+    .then(beer => {
+        const beerDetailDiv = document.querySelector("#beer-detail")
+        beerDetailDiv.innerHTML = `<h1>${beer.name}</h1>
+        <img src="${beer.image_url}">
+        <h3>${beer.tagline}</h3>
+        <textarea>${beer.description}</textarea>
+        <button id="edit-beer" class="btn btn-info">
+        Save
+        </button>`
+        beerDetailDiv.addEventListener("click", e => {
+            const newDescription = e.target.parentNode.querySelector("textarea").value
+            fetch(`${BEERS_URL}/${beer.id}`, {
+                method: "PATCH",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                  },
+                body: JSON.stringify({description: newDescription})
+            })
+        })
+    })
+}
+
